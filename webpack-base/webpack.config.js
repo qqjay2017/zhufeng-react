2866a57f6be74e5488c2d31bdefdcacea9e22d2b
Your Name <you@example.com>
2022-11-25 20:54:20 +0800

    feat: dev serve1111

--- webpack-base/webpack.config.js
+++ webpack-base/webpack.config.js
@@ -4,25 +4,24 @@
 const path = require("path");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const pageArr = ["index", "login"],
-entryObj = {},
-htmlPlugins = [];
+  entryObj = {},
+  htmlPlugins = [];
 
-pageArr.forEach(chunk=>{
-    entryObj[chunk] = `./src/${chunk}.js`;
-    htmlPlugins.push(
-        new HtmlWebpackPlugin({
-            template: `./public/${chunk}.html`,
-            filename: `${chunk}.html`,
-            minify: true,
-      
-            // 导入的资源名称
-            chunks: [chunk],
-          })
+pageArr.forEach((chunk) => {
+  entryObj[chunk] = `./src/${chunk}.js`;
+  htmlPlugins.push(
+    new HtmlWebpackPlugin({
+      template: `./public/${chunk}.html`,
+      filename: `${chunk}.html`,
+      minify: true,
 
-    )
-})
+      // 导入的资源名称
+      chunks: [chunk],
+    })
+  );
+});
 
 module.exports = {
   mode: "development",
@@ -32,9 +31,14 @@ module.exports = {
     filename: "[name].[hash:8].js",
     path: path.resolve(__dirname, "dist"),
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    ...htmlPlugins,
-    
-  ],
+  devServer: {
+    host: "127.0.0.1",
+    port:3000,
+    open:true,
+    hot:true,// 开启热更新,
+    compress:true ,// 开启服务器端的gzip压缩
+
+
+  },
+  plugins: [new CleanWebpackPlugin(), ...htmlPlugins],
 };
