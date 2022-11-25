dce7ddb0fddf1b04911b0c947d77e8a4e42500f0
Your Name <you@example.com>
2022-11-25 20:33:00 +0800

    feat: htmlPlugins

---
+++ webpack-base/src/login.js
@@ -0,0 +1,5 @@
+import sum from './A'
+import average from './B'
+
+console.log(sum(100, 200, 300, 400));
+console.log(average(100, 200, 300, 400));
--- webpack-base/webpack.config.js
+++ webpack-base/webpack.config.js
@@ -1,13 +1,40 @@
 /**
  * 自定义webpack打包规则
  */
-const path = require('path')
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const pageArr = ["index", "login"],
+entryObj = {},
+htmlPlugins = [];
+
+pageArr.forEach(chunk=>{
+    entryObj[chunk] = `./src/${chunk}.js`;
+    htmlPlugins.push(
+        new HtmlWebpackPlugin({
+            template: `./public/${chunk}.html`,
+            filename: `${chunk}.html`,
+            minify: true,
+      
+            // 导入的资源名称
+            chunks: [chunk],
+          })
+
+    )
+})
+
 module.exports = {
-    mode:'development',
-    entry: './src/index.js',
-    output: {
-        // 有助于强缓存的测试
-        filename: 'bundle.[name].[hash:8].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-}
+  mode: "development",
+  entry: entryObj,
+  output: {
+    // 有助于强缓存的测试
+    filename: "[name].[hash:8].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    ...htmlPlugins,
+    
+  ],
+};
