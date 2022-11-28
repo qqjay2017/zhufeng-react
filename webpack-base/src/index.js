dd6d299c052e6f0521adc7a43b4787432f34e9b5
Your Name <you@example.com>
2022-11-29 07:48:12 +0800

    Finish 图片处理与别名

--- webpack-base/src/index.js
+++ webpack-base/src/index.js
@@ -1,16 +1,22 @@
 import "@babel/polyfill"
-import sum from './A'
-import average from './B'
+import sum from '@/A'
+import average from '@/B'
 
-import './assets/reset.min.css'
-import './index.less'
+import '@/assets/reset.min.css'
+import '@/index.less'
 
 console.log(sum(10, 20, 30, 40));
 console.log(average(10, 20, 30, 40));
 
-const fn = ()=>{
-    console.log(fn());
-}
-fn()
+
 
 new Promise(()=>{})
+
+
+// js中处理静态图片, 需要先基于Es6Module规范导入进来,这样webpack才会对此图片进行打包
+
+import infoImg from '@/assets/images/inforec-20221127.jpg'
+
+let img = new Image()
+img.src = infoImg
+console.log(img,infoImg);
